96e921ba0d69d6f8c2eb626c882d4f12a6d41b13
Sathar Dinesh <satharudinesh212@gmail.com>
2023-10-31 08:16:59 +0530

    Time: 15 ms (49.58%) | Memory: 43.1 MB (27.97%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,20 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        if(nums.length<3){
-            return -1;
-        }
-        int res=Integer.MAX_VALUE;
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(res>Math.abs(nums[i]+nums[j]+nums[k]-target)){
-                        res=Math.abs(nums[i]+nums[j]+nums[k]-target);
-                        ans=nums[i]+nums[j]+nums[k];
-                    }
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum; // Update closest sum if the current sum is closer to the target
+                }
+
+                if (sum < target) {
+                    j++; // Increment j to increase the sum
+                } else {
+                    k--; // Decrement k to decrease the sum
                 }
             }
         }
-        return ans;
+
+        return closestSum;
     }
 }
