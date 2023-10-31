2f99fd900d690cdd00367b3dd7fd833b5ad94e5e
Sathar Dinesh <satharudinesh212@gmail.com>
2023-10-31 08:29:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (90.77%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,54 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
-
-    private Map<Character, String> digitToChar = Map.of(
-        '2',
-        "abc",
-        '3',
-        "def",
-        '4',
-        "ghi",
-        '5',
-        "jkl",
-        '6',
-        "mno",
-        '7',
-        "pqrs",
-        '8',
-        "tuv",
-        '9',
-        "wxyz"
-    );
+    private Map<Character, String> digitToLetters = new HashMap<>();
+    private List<String> resultList = new ArrayList<>();
 
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) {
-            return new ArrayList();
+        if (digits == null || digits.length() == 0) {
+            return resultList;
         }
 
-        List<String> ans = new ArrayList();
-        String cur = "";
-        backtrack(digits, ans, cur, 0);
-        return ans;
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+
+        generateCombinations(digits, 0, new StringBuilder());
+
+        return resultList;
+
+
     }
 
-    public void backtrack(
-        String digits,
-        List<String> ans,
-        String cur,
-        int index
-    ) {
-        if (cur.length() == digits.length()) {
-            ans.add(cur);
+
+    private void generateCombinations(String digits, int currentIndex, StringBuilder currentCombination) {
+        if (currentIndex == digits.length()) {
+            resultList.add(currentCombination.toString());
             return;
-        } else if (index >= digits.length()) {
-            return;
-        } else {
-            String digit = digitToChar.get(digits.charAt(index));
-            for (char c : digit.toCharArray()) {
-                backtrack(digits, ans, cur + c, index + 1);
+        }
+
+        char currentDigit = digits.charAt(currentIndex);
+        String letterOptions = digitToLetters.get(currentDigit);
+
+        if (letterOptions != null) {
+            for (int i = 0; i < letterOptions.length(); i++) {
+                char letter = letterOptions.charAt(i);
+                currentCombination.append(letter);
+                generateCombinations(digits, currentIndex + 1, currentCombination);
+                currentCombination.deleteCharAt(currentCombination.length() - 1);
             }
         }
     }
